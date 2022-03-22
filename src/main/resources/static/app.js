dc148b4bafaac572026de2a140397380767a2257
Jose Gamboa <jose.gamboa@mail.escuelaing.edu.co>
2022-03-22 17:27:24 -0500

    PARTE 1

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -35,9 +35,9 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
-                
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                var pointJSON=JSON.parse(eventbody.body);
+                alert("El punto agregado fue "+pointJSON.x+' '+pointJSON.y);
             });
         });
 
@@ -56,6 +56,7 @@ var app = (function () {
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt));
 
             //publicar el evento
         },
